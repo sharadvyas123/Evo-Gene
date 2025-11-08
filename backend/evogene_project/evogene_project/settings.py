81a1385a291569478a6c2e803424a0976f236929
Nagar-Nivedan <kaustubhsrivastavajee22@gmail.com>
2025-11-08 20:28:42 +0530

    Login and Register Page

--- backend/evogene_project/evogene_project/settings.py
+++ backend/evogene_project/evogene_project/settings.py
@@ -74,10 +74,16 @@ WSGI_APPLICATION = 'evogene_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'evogene_db',        
+        'USER': 'postgres',          
+        'PASSWORD': 'vaibhav2807', 
+        'HOST': 'localhost',         
+        'PORT': '5432',              
     }
 }
+
+
 
 
 # Password validation
--- backend/evogene_project/register/models.py
+++ backend/evogene_project/register/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class CustomUser(AbstractUser):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=128)
+    confirm_password = models.CharField(max_length=128)
+
+    username = None 
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['name']
+
+    def __str__(self):
+        return self.email
