d06d71b93f880e7e27a61ea0f4352c4444cbe8e0
sharadvyas123 <sharadvyas132@gmail.com>
2025-11-09 11:42:36 +0530

    Update next.config.mjs

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,9 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // This tells Next.js NOT to run tsc during the build
+  // This tells Next.js NOT to run tsc during the build.
+  // Recommended to remove this once all type errors are resolved.
   typescript: {
     ignoreBuildErrors: true,
   },
 };
 
-module.exports = nextConfig;
+// Use ES Module syntax for .mjs files
+export default nextConfig;
